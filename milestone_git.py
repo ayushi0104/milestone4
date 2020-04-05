5d4bc9162bd30cad14630b3008ce633f92ba97b1
Ayushi Shrivastava <ayushi.0104@gmail.com>
2020-04-05 17:35:35 +0530

    Final code (as per pep8 guidelines)

--- milestone_git.py
+++ milestone_git.py
@@ -1,17 +1,17 @@
 def fibonaccii(n):
-	if n==1:
-		print ("Fibonacci Series :\n0 ")
-	elif n==2:
-		print ("Fibonacci Series :\n 0 \n 1")
-	elif n>2:
-		print ("Fibonacci Series : \n 0 \n 1 ")
-		first = 0
-		second = 1
-		i=2
-		for i in range(2,n):
-			successive = first + second
-			print(successive)
-			first = second
-			second = successive
-n=int(input("Enter the range : "))
+    if n == 1:
+        print ("Fibonacci Series :\n 0 ")
+    elif n == 2:
+        print ("Fibonacci Series :\n 0 \n 1")
+    elif n > 2:
+        print ("Fibonacci Series : \n 0 \n 1 ")
+        first = 0
+        second = 1
+        i = 2
+        for i in range(2, n):
+            successive = first + second
+            print(successive)
+            first = second
+            second = successive
+n = int(input("Enter the range : "))
 fibonaccii(n)
