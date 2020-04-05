80faf7b0f50ca1cdcd0b06bb5c29f8c002d95630
Ayushi Shrivastava <ayushi.0104@gmail.com>
2020-04-05 17:03:45 +0530

    Rectifying the code; adding conditions

--- milestone_git.py
+++ milestone_git.py
@@ -1,9 +1,14 @@
 def fibonaccii(n):
-	first = 0
-	second = 1
-	for i in range(2,n):
-		successive = first + second
-		print(successive)
-		first = second
-		second = successive
-	
+	if n==1:
+		print ("0")
+	elif n==2:
+		print ("0 1")
+	elif n>2:
+		first = 0
+		second = 1
+		i=2
+		for i in range(2,n):
+			successive = first + second
+			print(successive)
+			first = second
+			second = successive
